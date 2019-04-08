42f547e04ac83f671c9f172032fcfa0521303f5f
CJHackerz <cjhackerz443@protonmail.com>
2019-04-08 18:48:06 +0530

    Added automated install script with updated docker file

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,31 @@
-FROM arm32v7/debian
+FROM multiarch/debian-debootstrap:armhf-stretch
 
 WORKDIR /root
 RUN mkdir workspace
 VOLUME [ "/root/workspace" ]
 
+RUN echo "Fetching neccessary packages"
 RUN apt update -y
 RUN apt upgrade -y
-RUN apt install build-essential python python-dev python-pip python3 python3-dev python3-pip cmake gdb wget gnupg2 git apache2 -y
+RUN apt install build-essential apt-utils systemd python python-dev python-pip python3 python3-dev python3-pip cmake pkg-config gdb wget gnupg2 git apache2 -y
+
+RUN echo "Installing gef dependencies"
 RUN pip3 install capstone==3.0.5.post1
-RUN pip3 install keystone-engine==0.9.1.post3
 RUN pip3 install pylint==2.3.0
 RUN pip3 install ropper==1.11.6
 RUN pip3 install unicorn==1.0.1
+
+RUN echo "Going to build keystone engine for gef"
+RUN git clone https://github.com/keystone-engine/keystone.git
+RUN cd keystone && mkdir build && cd build && ../make-share.sh && make -j4 && make install && ldconfig
+RUN cd keystone/bindings/python && make install3 && $(which python3) setup.py install
+RUN rm -drf keystone/build
+
+RUN echo "Installing gef"
 RUN wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef.sh | sh
+
+RUN echo "Getting firmwalker and firmware-mod-lit"
 RUN git clone https://github.com/craigz28/firmwalker
 RUN git clone https://github.com/rampageX/firmware-mod-kit.git
 
-CMD echo "Made by CJHackerz"
+CMD echo "Made by https://github.com/CJHackerz"
---
+++ launch.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+import os
+import subprocess
+
+class easy_iot:
+
+    def __init__(self):
+        print("##########################################")
+        print("Welcome to easy_iot-arm installer script")
+        print("##########################################")
+        print("> This script will install install all neccessary stuff to install arm based docker environment")
+    
+    def detectDocker(self):
+        if subprocess.getoutput("which docker") != '/usr/bin/docker':
+            print("[!]Error: Docker not found on the system")
+            return False
+        else:
+            return True
+
+    def installDocker(self):
+        if subprocess.getoutput('lsb_release -is') == 'Ubuntu':
+            print("[*]Going to install Docker on system, please enter the admin password if prompted")
+            os.system("sudo apt-get update")
+            os.system("sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common")
+            os.system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
+            os.system("sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'")
+            os.system("sudo apt update && udo apt-get install docker-ce docker-ce-cli containerd.io")
+            return True
+        
+        elif subprocess.getoutput('lsb_release -is') == 'Debian':
+            print("[*]Going to install Docker on system, please enter the admin password if prompted")
+            os.system("sudo apt-get update")
+            os.system("sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common")
+            os.system("curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -")
+            os.system("sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable'")
+            os.system("sudo apt update && udo apt-get install docker-ce docker-ce-cli containerd.io")
+            return True
+
+        else:
+            print("[!]Error: Current script only supports Debian and Ubuntu based distros")
+            return False
+    
+    def detectQemu(self):
+        if subprocess.getoutput('which qemu-arm') != '/usr/bin/qemu-arm' or subprocess.getoutput('which qemu-arm-static') != '/usr/bin/qemu-arm-static':
+            print("[!]Error: it seems qemu is not installed or some packages are missing")
+            return False
+        else:
+            return True
+    
+    def installQemu(self):
+        print("[*]Going to install qemu and binfmt-support for multiarch in docker")
+        a = subprocess.getstatusoutput("sudo apt install -y qemu qemu-user-static qemu-user binfmt-support -y")
+        b = subprocess.getstatusoutput("sudo docker run --rm --privileged multiarch/qemu-user-static:register")
+        if a[0] == 0 and b[0] == 0:
+            return True
+        else: 
+            False
+
+if __name__ == '__main__':
+    x = easy_iot()
+
+    if x.detectDocker() == False:
+        print("[!]Error: Could not find docker want to install it?[y/Y/n/N] ")
+        ans = input()
+        if ans == 'y' or ans == 'Y':print("[!]Error: Could not find docker want to install it?[y/Y/n/N] ")
+        ans = input()
+        if ans == 'y' or ans == 'Y':
+            status = x.installDocker()
+            if status == False:
+                print("[!]Error: Could not install docker can't continue")
+        else:
+            print("[!]Can't continue without docker, come back and rerun this script one you change your mind....")
+            status = x.installDocker()
+            if status == False:
+                print("[!]Error: Could not install docker can't continue")
+            else:
+                print("[!]Can't continue without docker, come back and rerun this script one you change your mind....")
+    elif x.detectQemu() == False:
+        print("[!]Error: Could not find qemu want to install it?[y/Y/n/N] ")
+        ans = input()
+        if ans == 'y' or ans == 'Y':
+            status = x.installQemu()
+            if status == False:
+                print("[!]Error: Could not install qemu can't continue")
+        else:
+            print("[!]Can't continue without qemu, come back and rerun this script one you change your mind....")
+    else:
+        print("[*]Everything seems fine, want to build or fetch new one?[b/f] ")
+        ans = input()
+        if ans == 'b':
+            print("[!]This is going to take some time have some coffee and comback :)")
+            os.system("sudo docker build -t cjhackerz/easy_iot:latest .")
+            os.system("mkdir workspace")
+            print("[*]Dropping you into container shell...")
+            os.system("sudo docker run -it -v $PWD/workspace:/root/workspace cjhackerz/easy_iot:latest /bin/bash")
+        elif ans == 'f':
+            print("[!]Going to pull container from docker hub, this requires internet connection...")
+            os.system("sudo docker pull cjhackerz/easy_iot:latest")
+            os.system("mkdir workspace")
+            print("[*]Dropping you into container shell...")
+            os.system("sudo docker run -it -v $PWD/workspace:/root/workspace cjhackerz/easy_iot:latest /bin/bash")
+        os.system()
