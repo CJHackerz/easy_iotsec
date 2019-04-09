6984390e872237e06d1c0b4c3952dc52f1360689
CJHackerz <cjhackerz443@protonmail.com>
2019-04-09 16:31:26 +0530

    Fixed docker install script for kali

--- scripts/docker_kali.sh
+++ scripts/docker_kali.sh
@@ -2,5 +2,6 @@
 sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+touch /etc/apt/source.list.d/docker.list
 echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" >> /etc/apt/source.list.d/docker.list
 sudo apt update && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
