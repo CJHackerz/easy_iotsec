67123c23ac4c2f1bff29c8ce3153257ef169dea9
CJHackerz <cjhackerz443@protonmail.com>
2019-04-09 12:12:20 +0530

    quotes problem

--- scripts/docker_debian.sh
+++ scripts/docker_debian.sh
@@ -2,5 +2,5 @@
 sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable'
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update -y && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
--- scripts/docker_kali.sh
+++ scripts/docker_kali.sh
@@ -2,5 +2,5 @@
 sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/debian stretch stable'
+echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" >> /etc/apt/source.list.d/docker.list
 sudo apt update && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
